e6c1c40c9d80b5ab225a26a30064eda33c657ad7
Shweta Shinde <shindeshweta1604@gmail.com>
2025-07-30 10:47:34 +0530

    First Commit Test

---
+++ src/main/java/Ecommerce_Automation/Ecommerce_Automation/App.java
@@ -0,0 +1,10 @@
+package Ecommerce_Automation.Ecommerce_Automation;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
